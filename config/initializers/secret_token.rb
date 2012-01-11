5b17f85996cec10d1aaf59fcbc961a506d854e63
Jeff Lam Tian Hung <jefflamth@gmail.com>
2012-01-11 16:30:07 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruuva::Application.config.secret_token = '8e7f9bb98a66795c01c045e6842f9cd1709de5b836e405c705369b445c61dff32576e909df2218d9536fcaed9054b66a2600164c308492a1aab8a83d9ce0592b'
