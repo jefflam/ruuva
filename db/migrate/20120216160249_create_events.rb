f868735d2a4816eeb0f74b51f795f76ec0d7a309
Jeff Lam Tian Hung <jefflamth@gmail.com>
2012-02-22 02:07:19 +0800

    repush to heroku

--- db/migrate/20120216160249_create_events.rb
+++ db/migrate/20120216160249_create_events.rb
@@ -1,13 +1,14 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.integer :date
-      t.integer :time
       t.string :description
       t.integer :collection_id
+      t.datetime :date_time
 
       t.timestamps
     end
     add_index :events, :collection_id
+    add_index :events, :date_time
+
   end
 end
